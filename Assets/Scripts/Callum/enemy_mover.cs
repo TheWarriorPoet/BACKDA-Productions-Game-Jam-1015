7d93d4043cc8106a9f64a1e665dcbda60aee9420
cufffs <callum.buerckner@hotmail.com>
2015-10-09 21:25:10 +1100

    basic enemy ai script

---
+++ Assets/Scripts/Callum/enemy_mover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy_mover : MonoBehaviour {
+
+	public float _speed = 1.0f;
+	public GameObject _platform;
+	
+
+	private bool _going_left = true;
+
+	void Start () {
+		this.transform.SetParent(_platform.transform, true);
+	}
+
+	void FixedUpdate () {
+		if(detect_ground())
+				this.transform.position = new Vector2(this.transform.position.x + (_going_left ? _speed : -_speed), this.transform.position.y);
+		else 
+			_going_left = !_going_left;
+	}
+
+	bool detect_ground()
+	{
+		Vector2 us = new Vector2(this.transform.position.x+(_going_left ? 0.5f : -0.5f), this.transform.position.y);
+		return (Physics2D.Raycast(us, new Vector2(us.x, us.y-2)).collider != null);
+	}
+}
