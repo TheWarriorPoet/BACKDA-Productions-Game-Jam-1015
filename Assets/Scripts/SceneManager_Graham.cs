acf4a0c553930e0c357cc14a7c342975fb7b2294
Arakash <fallendomion@hotmail.com>
2015-10-09 20:42:56 +1100

    Global Variables added to SceneManager_MainGame Script

--- Assets/Scripts/SceneManager_Graham.cs
+++ Assets/Scripts/SceneManager_Graham.cs
@@ -3,8 +3,32 @@ using System.Collections;
 
 public class SceneManager_Graham : SceneManager_Base {
 
-	// Use this for initialization
-	void Start () {
+    // Min and max health values, to clamp the player health and prevent bugs
+    // Read only access to both.
+    private int m_MaxHealth = 100;
+    public int maxHealth
+    {
+        get { return m_MaxHealth; }
+    }
+    private int m_MinHealth = 0;
+    public int minHealth
+    {
+        get { return m_MinHealth; }
+    }
+
+    // Global Health variable used by A Variety of Scripts to alter and retrieve health.
+    // Check with other programmers before changing the name of the parameter, or youll break scripts
+    private int m_PlayerHealth;
+    public int playerHealth
+    {
+        get { return m_PlayerHealth; }
+        // Value is clamped between min and max to prevent bugs
+        set { m_PlayerHealth = Mathf.Clamp(value, m_MinHealth, m_MinHealth); }
+    }
+
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
