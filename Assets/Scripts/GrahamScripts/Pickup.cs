302689abdae172cf37739c262b8883edba0cc68c
Arakash <fallendomion@hotmail.com>
2015-10-09 20:29:51 +1100

    Pickup Script Implementation

--- Assets/Scripts/GrahamScripts/Pickup.cs
+++ Assets/Scripts/GrahamScripts/Pickup.cs
@@ -3,23 +3,54 @@ using System.Collections;
 
 public class Pickup : MonoBehaviour {
 
+    public enum Type
+    {
+        eType1,
+        eType2,
+        eType3,
+        eType4
+    }
+
+    private Type m_PickupType = Type.eType1;
+    public Type pickupType
+    {
+        get { return m_PickupType; }
+        set { m_PickupType = value; }
+    }
 
     void OnTriggerEnter(Collider a_CollisionInfo)
     {
         if(a_CollisionInfo.tag == "Player")
         {
-            // Triger destruction of Pickup
-            // Do global stats modification of whatever is needed for pickup Type
+            // TO DO: Behaviour based on pickup types
+            switch(m_PickupType)
+            {
+                case Type.eType1:
+                {
+                    // Add global value modifications etc here
+                    break;
+                }
+                case Type.eType2:
+                {
+                    // Add global value modifications etc here
+                    break;
+                }
+                case Type.eType3:
+                {
+                    // Add global value modifications etc here
+                    break;
+                }
+                case Type.eType4:
+                {                  
+                    // Add global value modifications etc here
+                    break;
+                }
+                default:
+                        break;
+            }
+
+            // Should trigger destruction of pickup.
+            Destroy(this.gameObject);
         }
     }
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
