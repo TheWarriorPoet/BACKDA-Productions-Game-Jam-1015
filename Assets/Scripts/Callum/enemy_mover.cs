9d13144bad655b3911db7caa03ba012c24c0ab5b
cufffs <callum.buerckner@hotmail.com>
2015-10-10 13:12:39 +1100

    jumping enemies

--- Assets/Scripts/Callum/enemy_mover.cs
+++ Assets/Scripts/Callum/enemy_mover.cs
@@ -1,28 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class enemy_mover : MonoBehaviour {
 
-	public float _speed = 1.0f;
-	public GameObject _platform;
-	
+	public enum enemy_types {patrol, jump, jump_move};
+
+	public float _jump_height = 4.0f;
+	public float _speed = 0.05f;
+	public enemy_types _type = enemy_types.patrol;
+	public GameObject _platform;	
 
 	private bool _going_left = true;
+	private bool _last_left = true;
+	private Rigidbody2D _rb;
 
 	void Start () {
 		this.transform.SetParent(_platform.transform, true);
+		this._rb = this.GetComponent<Rigidbody2D>();
 	}
 
 	void FixedUpdate () {
-		if(detect_ground())
-				this.transform.position = new Vector2(this.transform.position.x + (_going_left ? _speed : -_speed), this.transform.position.y);
-		else 
-			_going_left = !_going_left;
+		switch(this._type){
+		case enemy_types.patrol:
+		{
+			if(detect_ground()){
+				this.transform.position = new Vector2(this.transform.position.x + (this._going_left ? this._speed : -this._speed), this.transform.position.y);
+			}
+			else 
+				this._going_left = !this._going_left;
+			break;
+		}
+		case enemy_types.jump:
+		{
+			if(this._rb.velocity.y.Equals(0.0f))
+				this._rb.AddForce(new Vector2(0.0f, this._jump_height), ForceMode2D.Impulse);
+			break;
+		}
+		case enemy_types.jump_move:
+		{
+			if(detect_ground()){
+				this.transform.position = new Vector2(this.transform.position.x + (this._going_left ? this._speed : -this._speed), this.transform.position.y);
+			}
+			else 
+				this._going_left = !this._going_left;	
+			if(this._rb.velocity.y.Equals(0.0f) && this._last_left !=this. _going_left)
+				this._rb.AddForce(new Vector2(0.0f, this._jump_height), ForceMode2D.Impulse);
+			break;
+		}
+		}
+		_last_left = _going_left;
 	}
-
 	bool detect_ground()
 	{
-		Vector2 us = new Vector2(this.transform.position.x+(_going_left ? 0.5f : -0.5f), this.transform.position.y);
+		Vector2 us = new Vector2(this.transform.position.x+(this._going_left ? 0.5f : -0.5f), this.transform.position.y);
 		return (Physics2D.Raycast(us, new Vector2(us.x, us.y-2)).collider != null);
 	}
 }
