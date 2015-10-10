b5228d2811d4888e4f161feecf78a54108787d39
Arakash <fallendomion@hotmail.com>
2015-10-10 17:35:05 +1100

    Pickup full implementation and image additions

--- Assets/Scripts/GrahamScripts/Pickup.cs
+++ Assets/Scripts/GrahamScripts/Pickup.cs
@@ -2,19 +2,73 @@
 using System.Collections;
 using System.Collections.Generic;
 
+//////////////////////////////////////////////////////////////////////
+// Author:  Graham Johnston
+// Date Created: 09/10/2015
+// Brief: Implements the spawning and destruction of pickups in the game.
+//     Handles the reaction when the Player collides with the pickup
+//     Handles applying the sprites and relevant health values to the
+//     pickups
+// Instructions: Set the type in the inspector or via the m_PickupType
+//      variable at runtime. Can be changed at any time before its destruction
+// Note :In the final version, can disable the runtime checks for if the type
+//      or scale was changed in the inspector
+//////////////////////////////////////////////////////////////////////
+
+[ExecuteInEditMode]
 public class Pickup : MonoBehaviour {
 
     [Tooltip("The scene manager in this scene")]
     public SceneManager_Base m_SceneManager;
+
+    [Tooltip("Sets the Type of Pickup")]
+    public Type m_PickupType;
+    private Type m_LastFramePickupType; // To detect if its changed in the inspector in runtime.
+
+    [Tooltip("Sets the Type of Pickup")]
+    [Range(0, 50)]
+    public int m_SizeScalar = 1;
+    private int m_SizeScalarLastFrame;
+
+    public Sprite m_Cabbage;
+    public Sprite m_Celery;
+    public Sprite m_CheeseBurger;
+    public Sprite m_Chips;
+    public Sprite m_Hotdog;
+    public Sprite m_Chocolate;
+    //public Sprite m_Carrot; // NO IMAGE
+    //public Sprite m_Sundae; // NO IMAGE
 
     public enum Type
     {
         eCheeseBurger, // 4%
         eHotDog, // 3%
         eChocolate, // 2%
-        eSundae, // 2%
+        //eSundae, // 2% // NO IMAGE
         eFries, // 2%
-        eCabbage // -3% (bad food)
+        eCabbage, // -3% (bad food)
+        eCelery // -3% (bad food)
+        //eCarrot // -2% (bad food) // NO IMAGE
+    }
+
+    void Start()
+    {
+        SetPickUpType(m_PickupType);
+        m_LastFramePickupType = m_PickupType;
+        m_SizeScalarLastFrame = m_SizeScalar;
+    }
+
+    void Update()
+    {
+        // Exists to alter the sprite during development if it changes during runtime
+        // can be removed in the final version.
+        if(m_LastFramePickupType != m_PickupType || m_SizeScalarLastFrame != m_SizeScalar)
+        {
+            // If the pickup type was changed via the public variable
+            SetPickUpType(m_PickupType);
+            m_LastFramePickupType = m_PickupType;
+            m_SizeScalarLastFrame = m_SizeScalar;
+        }
     }
 
     public struct PickupData
@@ -28,44 +82,63 @@ public class Pickup : MonoBehaviour {
         public string m_FileName;
     }
 
-    private Type m_PickupType = Type.eCheeseBurger;
-    public Type pickupType
+    private void SetPickUpType(Type a_eType)
     {
-        // Value should be set by the function, which spawns the appropriate asset to match the pickup
-        get { return m_PickupType; }
-    }
-
-    // Static storage of the filenames of each pickup type
-    static List<PickupData> m_AvailablePickups = new List<PickupData>();
-
-    void Awake()
-    {
-        // Initalises the pickuplist with prefab names for each of them
-        if(m_AvailablePickups.Count == 0)
+        // Sets teh pickup to its relevant sprite
+        // Scales the image to preserve the aspect ratio.
+        // Size is determined by m_SizeScalar
+        m_PickupType = a_eType;
+        GameObject attachedgameObject = this.gameObject;
+        switch (a_eType)
         {
-            // TO DO :Add in appropraite file names
-            m_AvailablePickups.Add(new PickupData(Type.eCheeseBurger, "none"));
-            m_AvailablePickups.Add(new PickupData(Type.eHotDog, "none"));
-            m_AvailablePickups.Add(new PickupData(Type.eChocolate, "none"));
-            m_AvailablePickups.Add(new PickupData(Type.eSundae, "none"));
-            m_AvailablePickups.Add(new PickupData(Type.eFries, "none"));
-            m_AvailablePickups.Add(new PickupData(Type.eCabbage, "none"));
+            case Type.eCabbage:
+                attachedgameObject.GetComponent<SpriteRenderer>().sprite = m_Cabbage;
+                attachedgameObject.transform.localScale = new Vector3(1.11054f, 1, 0) * 
+                    ((float)m_SizeScalar / 10.0f);
+                break;
+            case Type.eCelery:
+                attachedgameObject.GetComponent<SpriteRenderer>().sprite = m_Celery;
+                attachedgameObject.transform.localScale = new Vector3(0.60674f, 1, 0) * 
+                    ((float)m_SizeScalar / 10.0f);
+                break;
+            case Type.eCheeseBurger:
+                attachedgameObject.GetComponent<SpriteRenderer>().sprite = m_CheeseBurger;
+                attachedgameObject.transform.localScale = new Vector3(1.7f, 1, 0) * 
+                    ((float)m_SizeScalar / 10.0f);
+                break;
+            case Type.eChocolate:
+                attachedgameObject.GetComponent<SpriteRenderer>().sprite = m_Chocolate;
+                attachedgameObject.transform.localScale = new Vector3(0.73299f, 1, 0) * 
+                    ((float)m_SizeScalar / 10.0f);
+                break;
+            case Type.eFries:
+                attachedgameObject.GetComponent<SpriteRenderer>().sprite = m_Chips;
+                attachedgameObject.transform.localScale = new Vector3(0.55700f, 1, 0) * 
+                    ((float)m_SizeScalar / 10.0f);
+                break;
+            case Type.eHotDog:
+                attachedgameObject.GetComponent<SpriteRenderer>().sprite = m_Hotdog;
+                attachedgameObject.transform.localScale = new Vector3(1.33484f, 1, 0) * 
+                    ((float)m_SizeScalar / 10.0f);
+                break;
+            //case Type.eSundae: // NO Image
+            //    this.gameObject.GetComponent<SpriteRenderer>().sprite = m_Sundae;
+            //    break;
+            //case Type.eCarrot: // NO Image
+            //    this.gameObject.GetComponent<SpriteRenderer>().sprite = m_Carrot;
+            //    break;
+            default:
+                break;
         }
     }
 
-    public void SetPickUpType(Type a_eType)
+
+    void OnTriggerEnter2D(Collider2D a_Collider2D)
     {
-        m_PickupType = a_eType;
-        string associatedPrefabName = GetPickupFileName(a_eType);
-
-        // TO DO: Load in the appropriate asset based on the type and given filename
-    }
-
-
-    void OnTriggerEnter(Collider a_CollisionInfo)
-    {
-        if(a_CollisionInfo.tag == "Player")
+        if(a_Collider2D.tag == "Player")
         {
+            // This is a hack to allow this to work in both the main scene and my test scene.
+            // In the final version, will just use mainGame public variable.
             SceneManager_Graham m_RefSceneManagerGraham = null;
             SceneManager_MainGame m_RefSceneManagerMain = null;
             if (m_SceneManager.name == "SceneManager")
@@ -79,51 +152,56 @@ public class Pickup : MonoBehaviour {
                 m_RefSceneManagerGraham = m_SceneManager as SceneManager_Graham;
             }
 
-            // TO DO: Behaviour based on pickup types
             switch(m_PickupType)
             {
                 case Type.eCheeseBurger:
                 {
                     if(m_RefSceneManagerGraham != null) { m_RefSceneManagerGraham.playerHealth += 4; };
                     if (m_RefSceneManagerMain != null) { m_RefSceneManagerMain.playerHealth += 4; };
-                    // Add global value modifications etc here
                     break;
                 }
                 case Type.eHotDog:
                 {
                     if (m_RefSceneManagerGraham != null) { m_RefSceneManagerGraham.playerHealth += 3; };
                     if (m_RefSceneManagerMain != null) { m_RefSceneManagerMain.playerHealth += 3; };
-                    // Add global value modifications etc here
                     break;
                 }
                 case Type.eChocolate:
                 {
                     if (m_RefSceneManagerGraham != null) { m_RefSceneManagerGraham.playerHealth += 2; };
                     if (m_RefSceneManagerMain != null) { m_RefSceneManagerMain.playerHealth += 2; };
-                    // Add global value modifications etc here
-                    break;
-                }
-                case Type.eSundae:
-                {
-                    if (m_RefSceneManagerGraham != null) { m_RefSceneManagerGraham.playerHealth += 2; };
-                    if (m_RefSceneManagerMain != null) { m_RefSceneManagerMain.playerHealth += 2; };
-                    // Add global value modifications etc here
                     break;
                 }
                 case Type.eFries:
                 {
                     if (m_RefSceneManagerGraham != null) { m_RefSceneManagerGraham.playerHealth += 2; };
                     if (m_RefSceneManagerMain != null) { m_RefSceneManagerMain.playerHealth += 2; };
-                    // Add global value modifications etc here
                     break;
                 }
                 case Type.eCabbage:
                 {
-                    if (m_RefSceneManagerGraham != null) { m_RefSceneManagerGraham.playerHealth -= 3; };
-                    if (m_RefSceneManagerMain != null) { m_RefSceneManagerMain.playerHealth -= 3; };
-                    // Add global value modifications etc here
+                    if (m_RefSceneManagerGraham != null) { m_RefSceneManagerGraham.playerHealth -= 5; };
+                    if (m_RefSceneManagerMain != null) { m_RefSceneManagerMain.playerHealth -= 5; };
                     break;
                 }
+                case Type.eCelery:
+                {
+                    if (m_RefSceneManagerGraham != null) { m_RefSceneManagerGraham.playerHealth -= 3; };
+                    if (m_RefSceneManagerMain != null) { m_RefSceneManagerMain.playerHealth -= 3; };
+                    break;
+                }
+                //case Type.eSundae: // NO IMAGE
+                //{
+                //    if (m_RefSceneManagerGraham != null) { m_RefSceneManagerGraham.playerHealth += 2; };
+                //    if (m_RefSceneManagerMain != null) { m_RefSceneManagerMain.playerHealth += 2; };
+                //    break;
+                //}
+                //case Type.eCarrot:  // NO IMAGE
+                //{
+                //    if (m_RefSceneManagerGraham != null) { m_RefSceneManagerGraham.playerHealth -= 3; };
+                //    if (m_RefSceneManagerMain != null) { m_RefSceneManagerMain.playerHealth -= 3; };
+                //    break;
+                //}
                 default:
                         break;
             }
@@ -131,17 +209,5 @@ public class Pickup : MonoBehaviour {
             // Should trigger destruction of pickup.
             Destroy(this.gameObject);
         }
-    }
-
-    string GetPickupFileName(Type a_PickupType)
-    {
-        foreach (var PickUp in m_AvailablePickups)
-        {
-            if (m_PickupType == PickUp.m_eType)
-            {
-                return PickUp.m_FileName;
-            }
-        }
-        return " ";
     }
 }
