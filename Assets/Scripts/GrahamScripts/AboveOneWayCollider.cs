0a67daea476e7cc635ace08c6f69e185776ccd83
Arakash <fallendomion@hotmail.com>
2015-10-10 20:32:28 +1100

    OneWayEdgeCollider

---
+++ Assets/Scripts/GrahamScripts/AboveOneWayCollider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//////////////////////////////////////////////////////////////////////
+// Author: Graham Johnston
+// Date Created: 10/10/2015
+// Brief:  A Very Simple class, designed to be combined with the OneWayCollider
+//      What this class stores is a bool that reports whether you have entered
+//      or left its trigger.
+//      The idea is, unless it returns true, you turn the collider off on
+//      the one way object.
+//      In effect, this means unless your colliding with both objects,
+//      it will ignore the collision.
+//////////////////////////////////////////////////////////////////////
+
+public class AboveOneWayCollider : MonoBehaviour {
+
+    private bool m_IsAbove;
+    public bool isAbove
+    {
+        get { return m_IsAbove; } // Read only public access
+    }
+
+    void OnTriggerEnter2D()
+    {
+        m_IsAbove = true;
+    }
+    void OnTriggerExit2D()
+    {
+        m_IsAbove = false;
+    }
+}
---
+++ Assets/Scripts/GrahamScripts/OneWayCollider.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//////////////////////////////////////////////////////////////////////
+// Author: Graham Johnston
+// Date Created: 10/10/2015
+// Brief:  Designed to be combined with the AboveOneWayCollider script
+//      to implement a one way platform.
+//      The concept is that unless the player is in contact with the "above"   
+//      trigger, then the collision is disabled.
+//////////////////////////////////////////////////////////////////////
+
+public class OneWayCollider : MonoBehaviour {
+
+    [Tooltip("The collider that sits above to make collision only trigger from one direction ")]
+    public AboveOneWayCollider m_AboveTrigger;
+
+    // Stores the object whose collision we are going to ignore if they collide from the wrong direction.
+    private Collider2D m_ColliderIgnoredObject;
+    private Collider2D m_ColliderThisObject;
+
+    private bool m_CollisionDisabled = false;
+
+	// Use this for initialization
+	void Start () {
+        m_ColliderThisObject = this.gameObject.GetComponent<Collider2D>();
+	}
+
+	void Update () {
+
+        // If we trigger the "above" trigger again and weve already disabled collision, enable the collision
+        if (m_CollisionDisabled == true && m_AboveTrigger.isAbove == true) 
+        {
+            m_CollisionDisabled = false;
+            Physics2D.IgnoreCollision(m_ColliderIgnoredObject, m_ColliderThisObject, false);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D a_CollisionInfo)
+    {
+        if (!m_AboveTrigger.isAbove) // If they are not in contact with the "above" trigger
+        {
+            m_ColliderIgnoredObject = a_CollisionInfo.collider; // store the object so we can enable the collision again later.
+            Physics2D.IgnoreCollision(m_ColliderIgnoredObject, m_ColliderThisObject);
+            m_CollisionDisabled = true;
+        }
+    }
+}
