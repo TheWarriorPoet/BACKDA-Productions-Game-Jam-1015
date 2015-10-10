7f8d1ead32b8bb01bcd93d4245deea9bbe4c00ef
Arakash <fallendomion@hotmail.com>
2015-10-10 22:45:12 +1100

    Win Conditions Sofa Prefab Main Scene Implementation of Win Conditions Main Scene Hierarchy cleaned up

---
+++ Assets/Scripts/GrahamScripts/HasCollided.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//////////////////////////////////////////////////////////////////////
+// Author: Graham Johnston
+// Date Created: 10/10/2015
+// Brief:  Stores whether the object collided with the player or not
+//      If it does collide with the player, it turns off its ability
+//      to collide with the player via the Physics engine.
+//      The effect is intended to allow the object to fall and hit terrain
+//      under gravity, while still allowing it to behave like a trigger 
+//      and allow the player to pass through it, without switching its layer.
+//////////////////////////////////////////////////////////////////////
+
+public class HasCollided : MonoBehaviour {
+
+    private Collider2D m_AttachedCollider;
+
+    private bool m_HasCollided = false;
+    public bool hasCollided
+    {
+        get { return m_HasCollided; }
+    }
+
+    void Start()
+    {
+        m_AttachedCollider = this.gameObject.GetComponent<Collider2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D a_CollisionData)
+    {
+        // First check if weve already collided, in which case skip the tag search
+        // Then see if the other object is the player
+        if(m_HasCollided == false && a_CollisionData.gameObject.tag == "Player")
+        {
+            m_HasCollided = true;
+            // Disables any further collision effects between this object and the player.
+            Physics2D.IgnoreCollision(m_AttachedCollider, a_CollisionData.collider);
+        }
+    }
+}
