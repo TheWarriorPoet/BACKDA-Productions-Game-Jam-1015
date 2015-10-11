8f3873499ca5a7b2c56ff3ee13937a0bec1ba85d
Arakash <fallendomion@hotmail.com>
2015-10-11 15:57:49 +1100

    Build Settings and WinScreen

---
+++ Assets/Scripts/GrahamScripts/WinScreen.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WinScreen : MonoBehaviour {
+
+    private int m_FrameNumber = 0;
+
+    public Sprite Frame2;
+    public Sprite Frame3;
+
+    private float m_TimeBetweenFrames = 0.7f;
+    private float m_AccumulatedFrameSwitchTime = 0;
+
+	// Update is called once per frame
+	void Update () {
+        m_AccumulatedFrameSwitchTime += Time.deltaTime;
+        if(m_AccumulatedFrameSwitchTime > m_TimeBetweenFrames)
+        {
+            m_AccumulatedFrameSwitchTime = 0;
+            if (m_FrameNumber <= 2)
+            {
+                m_FrameNumber++;
+            }
+            else
+            {
+                Application.LoadLevel("MainMenu");
+            }
+            
+            if(m_FrameNumber == 1)
+            {
+                this.gameObject.GetComponent<Image>().sprite = Frame2;
+            }
+            else if(m_FrameNumber == 2)
+            {
+                this.gameObject.GetComponent<Image>().sprite = Frame3;
+            }
+        }
+
+
+	}
+}
